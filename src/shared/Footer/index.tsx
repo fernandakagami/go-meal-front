c96f81c36b0d98f9588a3a9a55cc593d71b103da
Fernanda <fernandakagami@gmail.com>
2024-10-27 17:38:25 -0300

    add info in footer

--- src/shared/Footer/index.tsx
+++ src/shared/Footer/index.tsx
@@ -6,6 +6,15 @@ export function Footer() {
 
   return (
     <footer className={styles["footer-container"]}>
+
+      <div className={styles["link-container"]}>
+        <h4 className={styles["link-title"]}>Sobre a empresa</h4>
+        <ul>
+          <li className={styles["link"]}>Sobre nós</li>
+          <li className={styles["link"]}>Termos e condições</li>
+        </ul>
+      </div>
+
       <div className={styles["footer-wrapper"]}>
         <p className={styles.copyright}>All rights Reserved <Copyright size={16} /> Fernanda {currentDate} </p>
 
